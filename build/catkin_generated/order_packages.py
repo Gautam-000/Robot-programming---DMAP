d4aaddd5fc97211b360aacabb610fed7049ed361
Gautam-000 <146574211+Gautam-000@users.noreply.github.com>
2025-02-06 21:05:30 +0100

    Initial basic files +rp stuff
    I have added basic build, devel, src file

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/malik/dmap_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/malik/dmap_ws/devel;/home/malik/ros_project/devel;/opt/ros/noetic'.split(';') if '/home/malik/dmap_ws/devel;/home/malik/ros_project/devel;/opt/ros/noetic' != '' else []
